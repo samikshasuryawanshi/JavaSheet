165768c1d13ab4f55977e5ba5f0b1b741ddb1c5d
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-08-05 11:52:24 +0530

    Refactor graph initialization in BellManFord class; update edges for correct graph representation.

--- Graphs/BellManFord.java
+++ Graphs/BellManFord.java
@@ -18,16 +18,15 @@ class BellManFord{
 
         graph[0].add(new Edge(0,1,2));
         graph[0].add(new Edge(0,2,4));
+        
+        graph[1].add(new Edge(1,3,-4));
+        
+        graph[2].add(new Edge(2,3,2));
+        
+        graph[3].add(new Edge(3,4,4));
+        graph[4].add(new Edge(4,1,-1));
 
-        graph[1].add(new Edge(1,3,7));
-        graph[1].add(new Edge(1,2,1));
 
-        graph[2].add(new Edge(2,4,3));
-
-        graph[3].add(new Edge(3,5,1));
-
-        graph[4].add(new Edge(4,3,2));
-        graph[4].add(new Edge(4,5,5));
     }
 
 }
