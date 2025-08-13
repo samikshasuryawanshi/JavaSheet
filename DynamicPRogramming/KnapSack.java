8ffb92329fd100452ac2604e70a6d9693e835b6d
samiksha suryawanshi <suryawanshisamiksha506@gmail.com>
2025-08-13 11:30:48 +0530

    Implement tabulation method for KnapSack problem; update main execution logic for consistency.

--- DynamicPRogramming/KnapSack.java
+++ DynamicPRogramming/KnapSack.java
@@ -42,8 +42,36 @@ class KnapSack{
     }
 
     public static int knapSackTab(int val[],int wt[],int W){
-        int dp[][] = new int[val.length + 1][W+1];
-        
+        int n = val.length;
+        int dp[][] = new int[n + 1][W+1];
+        for(int i = 0;i<dp.length;i++){//0th col
+            dp[i][0] = 0;
+        }
+        for(int j = 0;j<dp[0].length;j++){//0th row
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1;i<n+1;i++){
+            for(int j = 1;j<W+1;j++){
+                int v = val[i-1];
+                int w = wt[i-1];
+
+                if(w <= j){
+                    //include
+                    int inProfit = v + dp[i-1][j-w];
+                    int exProfit = dp[i-1][j];
+
+                    dp[i][j] = Math.max(inProfit, exProfit);
+                }else{
+                    int exProfit = dp[i-1][j];
+
+                    dp[i][j] = exProfit;
+ 
+                }
+            }
+        }
+        return dp[n][W];
+
     }
 
 
@@ -62,7 +90,8 @@ class KnapSack{
             }}
 
        System.out.println("Maximum value in Knapsack = " + knapSackRec(W, wt, val, val.length));
-         System.out.println("Maximum value in Knapsack (Memoization) = " + knapSackMemo(W, wt, val, val.length, dp));
+       System.out.println("Maximum value in Knapsack (Memoization) = " + knapSackMemo(W, wt, val, val.length, dp));
+       System.out.println("Max value = " + knapSackTab(val, wt, W));
 
     }
 }
